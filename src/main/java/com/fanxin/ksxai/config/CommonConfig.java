0efd44289c5a215a117161ae90fc360650b1c31a
ksx919 <1983262652@qq.com>
2025-07-15 17:41:56 +0800

    0715

--- src/main/java/com/fanxin/ksxai/config/CommonConfig.java
+++ src/main/java/com/fanxin/ksxai/config/CommonConfig.java
@@ -1,19 +1,31 @@
 package com.fanxin.ksxai.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CommonConfig {
+
     @Bean
-    public ChatClient chatClient(OllamaChatModel model){
+    public ChatMemory chatMemory(){
+        return new InMemoryChatMemory();
+    }
+
+    @Bean
+    public ChatClient chatClient(OllamaChatModel model,ChatMemory chatMemory){
         return ChatClient
                 .builder(model)
-                .defaultSystem("你是一个非常严厉的老师，请你非常严厉的语气来回答问题。")
-                .defaultAdvisors(new SimpleLoggerAdvisor())
+                .defaultSystem("你是一个可爱的虚拟助手，名字叫小团团。")
+                .defaultAdvisors(
+                        new SimpleLoggerAdvisor(),
+                        new MessageChatMemoryAdvisor(chatMemory)
+                        )
                 .build();
     }
 }
--- src/main/java/com/fanxin/ksxai/controller/ChatController.java
+++ src/main/java/com/fanxin/ksxai/controller/ChatController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
+import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.CHAT_MEMORY_CONVERSATION_ID_KEY;
+
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/ai")
@@ -14,9 +16,10 @@ public class ChatController {
     private final ChatClient chatClient;
 
     @RequestMapping(value = "/chat",produces = "text/html;charset=utf-8")
-    public Flux<String> chat(String prompt){
+    public Flux<String> chat(String prompt, String chatId){
         return chatClient.prompt()
                 .user(prompt)
+                .advisors(a -> a.param(CHAT_MEMORY_CONVERSATION_ID_KEY, chatId))
                 .stream()
                 .content();
     }
